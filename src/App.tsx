989b4a46752cc1aa217c6f991388409c14989e72
QuangLe1710 <160602994+QuangLe1710@users.noreply.github.com>
2025-04-24 01:58:05 +0700

    sieuDepZai

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Hello World & Lê Minh Quang</h1>
+      <h1>Hello World & Lê Minh Quang Siêu cấp đẹp trai</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
