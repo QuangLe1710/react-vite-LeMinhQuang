8d8d767d2ad97494ffa6abfa724c7cf4ae6a50e8
QuangLe1710 <160602994+QuangLe1710@users.noreply.github.com>
2025-04-24 17:03:33 +0700

    31 Variable with JSX

--- src/components/learn/MySecondComponent.jsx
+++ src/components/learn/MySecondComponent.jsx
@@ -11,17 +11,37 @@ const MyThirdComponent = () => {
 //JSX
 // Fragment
 const MySecondComponent = () => {
+    const quang = "Quang đẹp trai no1 vũ trụ"
+    const arr = [1, 2, 3]
+    const Obj = {
+        name: "Quang",
+        age: 21
+    }
     return (
         <>
             <div>
-            My Second Component
+                My Second Component
             </div>
             <div className="child"
-                style={{borderRadius: "10px"}}
-            >child</div>
+                style={{ borderRadius: "10px" }}
+            >Tên trùm Zoe : {quang}</div>
+
+            <div className="child"
+                style={{ borderRadius: "10px" }}
+            >Mảng Zoe : {arr}</div>
+
+            <div className="child"
+                style={{ borderRadius: "10px" }}
+            >Đối tượng Zoe nhưng chỉ gọi được mỗi string : {Obj.name}</div>
+
+            <div className="child"
+                style={{ borderRadius: "10px" }}
+            >Đối tượng Zoe gọi được toàn bộ thông tin object : {JSON.stringify(Obj)}</div>
+
+            <div>{console.log(Obj)}</div>
         </>
-        
+
     )
 }
 
-export {MySecondComponent , MyThirdComponent};
+export { MySecondComponent, MyThirdComponent };
